dbe6a14614334d34bce7dc3f736d07cd6d139679
Ashlord@Sporadicity <ashlord@outlook.com>
2018-03-11 14:57:18 +0800

    day 9

---
+++ src/day9.nim
@@ -0,0 +1,36 @@
+var s = readFile("data/day9_input.txt")
+
+var groups = @[0]
+
+var currentLevel = 0
+var isInGarbage = false
+var isCancel = false
+
+for i, c in s:
+  if isCancel:
+    isCancel = false
+    continue
+  if isInGarbage:
+    if c == '>':
+      isInGarbage = false
+    elif c == '!':
+      isCancel = true
+    continue
+  case c:
+    of '{':
+      currentLevel += 1
+      if currentLevel > (groups.len() - 1):
+        groups.add(0)
+    of '}':
+      groups[currentLevel] += 1
+      currentLevel -= 1
+    of '<':
+      isInGarbage = true
+    else:
+      discard
+
+var sum = 0
+for i, n in groups:
+  sum += i * n
+
+echo sum
